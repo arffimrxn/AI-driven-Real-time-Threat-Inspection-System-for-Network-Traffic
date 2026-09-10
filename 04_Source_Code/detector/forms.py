6741c995af84bbbd2d36f398819893ebcb00ee42
daus-Onn <firdaushairumaini@gmail.com>
2026-09-10 15:27:19 +0800

    feat(results): add dashboard visual and evaluation metrics

--- 04_Source_Code/detector/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-
-class NetworkCaptureUploadForm(forms.Form):
-    capture_file = forms.FileField(
-        label="Select PCAP or CSV File",
-        widget=forms.FileInput(attrs={'class': 'form-control', 'accept': '.pcap,.pcapng,.csv'})
-    )
